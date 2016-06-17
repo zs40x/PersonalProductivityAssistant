f8b7406983227499b12e23c029192753ebe6b64a
Stefan Mehnert <StefanMehnert@gmail.com>
2016-06-17 14:55:12 +0200

    Initial Commit

---
+++ PersonalProductivityAssistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonalProductivityAssistant
+//
+//  Created by Stefan Mehnert on 17/06/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
