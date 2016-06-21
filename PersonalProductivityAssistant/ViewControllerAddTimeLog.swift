c90264a477d17260776cf014e40deb097f0643f8
Stefan Mehnert <StefanMehnert@gmail.com>
2016-06-21 20:29:59 +0200

    Added a start time picker in the log time view

--- PersonalProductivityAssistant/ViewControllerAddTimeLog.swift
+++ PersonalProductivityAssistant/ViewControllerAddTimeLog.swift
@@ -34,16 +34,7 @@ class ViewControllerAddTimeLog: UIViewController {
     }
     
 
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
     }
@@ -63,7 +54,6 @@ class ViewControllerAddTimeLog: UIViewController {
             performSegueWithIdentifier("UnwindToMainView", sender: self)
         }
     }
-    
     
     func getTimeLogData() -> TimeLogData {
         return TimeLogData(Activity: textEditActivity.text!)
