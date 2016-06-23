0e30c53e9e4e68a035cbdc3978e2fa9b03b503fc
Stefan Mehnert <StefanMehnert@gmail.com>
2016-06-23 17:33:30 +0200

    Uses a struct TimeLogData to pass the Informations about a new timeLog from the GUI to the model. should persist the start/from date/time

---
+++ PersonalProductivityAssistant/TimeLogData.swift
@@ -0,0 +1,9 @@
+//
+//  TimeLogData.swift
+//  PersonalProductivityAssistant
+//
+//  Created by Stefan Mehnert on 23/06/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
