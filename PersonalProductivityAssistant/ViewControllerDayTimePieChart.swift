d88d12819d4d47d4af3091a4b54a16ad7d5e773b
Stefan Mehnert <StefanMehnert@gmail.com>
2016-06-29 19:58:18 +0200

    displays a pie chart with mock data

--- PersonalProductivityAssistant/ViewControllerDayTimePieChart.swift
+++ PersonalProductivityAssistant/ViewControllerDayTimePieChart.swift
@@ -9,11 +9,18 @@
 import UIKit
 
 class ViewControllerDayTimePieChart: UIViewController {
-
+    
+    @IBOutlet weak var pieChartView: PieChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let months = ["Free", "Spent"]
+        let unitsSold = [20.0, 4.0]
+        
+        setChart(months, values: unitsSold)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +28,7 @@ class ViewControllerDayTimePieChart: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBOutlet weak var pieChartView: PieChartView!
+    
 
     /*
     // MARK: - Navigation
@@ -33,4 +40,28 @@ class ViewControllerDayTimePieChart: UIViewController {
     }
     */
 
+    func setChart(dataPoints: [String], values: [Double]) {
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let pieChartDataSet = PieChartDataSet(yVals: dataEntries, label: "Free vs. spent time")
+        let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet)
+        pieChartView.data = pieChartData
+        
+        var colors: [UIColor] = []
+        
+        for _ in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        pieChartDataSet.colors = colors    }
 }
