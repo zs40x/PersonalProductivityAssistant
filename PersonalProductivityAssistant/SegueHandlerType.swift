a26345150d9fcd74fd8f9667105125994941372a
Stefan Mehnert <StefanMehnert@gmail.com>
2016-07-01 06:48:34 +0200

    Applied protocol oriented identifiers for segues: https://www.natashatherobot.com/protocol-oriented-segue-identifiers-swift/

---
+++ PersonalProductivityAssistant/SegueHandlerType.swift
@@ -0,0 +1,9 @@
+//
+//  SegueHandlerType.swift
+//  PersonalProductivityAssistant
+//
+//  Created by Stefan Mehnert on 01/07/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
