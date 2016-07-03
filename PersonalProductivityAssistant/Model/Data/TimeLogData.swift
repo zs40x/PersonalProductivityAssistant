f582b02c524e1701fd400328592d75a0d4e67212
Stefan Mehnert <StefanMehnert@gmail.com>
2016-07-03 09:33:23 +0200

    Restructered files in the project directory

---
+++ PersonalProductivityAssistant/Model/Data/TimeLogData.swift
@@ -0,0 +1,15 @@
+//
+//  TimeLogData.swift
+//  PersonalProductivityAssistant
+//
+//  Created by Stefan Mehnert on 23/06/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
+
+struct TimeLogData {
+    var Activity: String
+    var From: NSDate
+    var Until: NSDate
+}
