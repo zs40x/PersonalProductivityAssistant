acf664dd8e6e7b32fc0d5f58cfb20790fcbdd98c
Stefan Mehnert <StefanMehnert@gmail.com>
2016-07-12 07:48:05 +0200

    Cleaned UI tests, deleted generated default comments

--- PersonalProductivityAssistantUITests/PersonalProductivityAssistantUITests.swift
+++ PersonalProductivityAssistantUITests/PersonalProductivityAssistantUITests.swift
@@ -22,15 +22,10 @@ class PersonalProductivityAssistantUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+     
         app.launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-        
         activityInputField = app.textFields["textEditActivity"]
         toolbarAddActivityButton = app.toolbars.buttons["Log Time"]
         datePickerFrom = app.datePickers.elementBoundByIndex(0)
@@ -38,7 +33,6 @@ class PersonalProductivityAssistantUITests: XCTestCase {
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
         
         app.terminate()
