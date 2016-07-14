a626f082209efb91d7b66327589d42f39f8a0d89
Stefan Mehnert <StefanMehnert@gmail.com>
2016-07-14 07:34:44 +0200

    Redesigned TimeLogView without date pickers, autoComplete tableView, which scales the layout when visible. Still with autoLayout issues

---
+++ PersonalProductivityAssistant/ViewControllers/DateTimePickViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DateTimePickViewController.swift
+//  PersonalProductivityAssistant
+//
+//  Created by Stefan Mehnert on 14/07/16.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import UIKit
+
+class DateTimePickViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
