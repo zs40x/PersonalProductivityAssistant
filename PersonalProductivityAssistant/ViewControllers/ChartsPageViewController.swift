d1d8263af7a5076fed2abcdefe4de7c061f22fd0
Stefan Mehnert <StefanMehnert@gmail.com>
2016-07-15 20:44:39 +0200

    Working UIPageViewController wich toggles between two pie chart views

--- PersonalProductivityAssistant/ViewControllers/ChartsPageViewController.swift
+++ PersonalProductivityAssistant/ViewControllers/ChartsPageViewController.swift
@@ -9,11 +9,37 @@
 import UIKit
 
 class ChartsPageViewController: UIPageViewController {
+    
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newPieChartViewController(ChartSourceHashtags()),
+                self.newPieChartViewController(ChartSourceFreeVsUsedTime())]
+    }()
+    
+    private func newPieChartViewController(chartDataValueProvider: ChartDataValueProvider) -> PieChartViewController {
+        
+        let uiViewController =
+            UIStoryboard(name: "Main", bundle: nil)
+                .instantiateViewControllerWithIdentifier("PieChartViewController")
+        
+        let pieChartViewController = uiViewController as! PieChartViewController
+        pieChartViewController.chartDataValueProvider = chartDataValueProvider
+        
+        return pieChartViewController
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .Forward,
+                               animated: true,
+                               completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +58,49 @@ class ChartsPageViewController: UIPageViewController {
     }
     */
 
+}
+
+extension ChartsPageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(pageViewController: UIPageViewController,
+                            viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController,
+                            viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
 }
