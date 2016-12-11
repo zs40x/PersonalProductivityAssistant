99f6b499e109de2b457b19d92bf7463ed3615d18
Stefan Mehnert <StefanMehnert@gmail.com>
2016-12-11 21:15:09 +0100

    Extracted cloudKit download and import in a new class TimeLogsInCKDownload

---
+++ PersonalProductivityAssistant/TimeLogsInCKDownload.swift
@@ -0,0 +1,9 @@
+//
+//  TimeLogsInCKDownload.swift
+//  PersonalProductivityAssistant
+//
+//  Created by Stefan Mehnert on 11/12/2016.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
