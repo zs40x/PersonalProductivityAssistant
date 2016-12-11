ffc4687db77aaba93355b5a1c7434be17b6322e4
Stefan Mehnert <StefanMehnert@gmail.com>
2016-12-11 21:23:04 +0100

    Fixed typo

--- PersonalProductivityAssistant/CloudKit/TimeLogsInCK.swift
+++ PersonalProductivityAssistant/CloudKit/TimeLogsInCK.swift
@@ -17,7 +17,7 @@ class TimeLogsInCK {
 
     private let timeLogRepository = TimeLogRepository()
     private let cloudKitContainer = CKContainer.default()
-    private let recordTypeTimeLogs = "TimeLogs"
+    public static let RecordTypeTimeLogs = "TimeLogs"
     
     var dataSyncCompletedDelegate: CKDataSyncCompletedDelegate?
     
@@ -45,7 +45,7 @@ class TimeLogsInCK {
         let recordUUID = timeLog.uuid!
         
      
-        let ckrTimeLog = CKRecord(recordType: recordTypeTimeLogs, recordID: CKRecordID(recordName: recordUUID))
+        let ckrTimeLog = CKRecord(recordType: TimeLogsInCK.RecordTypeTimeLogs, recordID: CKRecordID(recordName: recordUUID))
         
         if let activity = timeLog.activity {
             ckrTimeLog.setObject(activity as NSString, forKey: "activity")
@@ -97,59 +97,9 @@ class TimeLogsInCK {
         
         NSLog("TimeLogsInCK.importTimeLogsFromCkToDb()")
         
-        let predicate = NSPredicate(value:true)
-        let query = CKQuery(recordType: self.recordTypeTimeLogs, predicate: predicate)
-        
-        cloudKitContainer.privateCloudDatabase.perform(query, inZoneWith: nil, completionHandler: { (records, error) in
-            
-            if let error = error {
-                NSLog("Saving to iCloud failed: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let records = records else { return }
-            
-            NSLog("CK - fetched \(records.count)")
-            
-            let timeLogRepository = TimeLogRepository()
-            
-            for ckTimeLog in records {
-                
-                let activity = ckTimeLog.object(forKey: "activity") as? String ?? ""
-                let from = ckTimeLog.object(forKey: "from") as? Date ?? Date()
-                let until = ckTimeLog.object(forKey: "until") as? Date ?? Date()
-                let uuid = UUID.init(uuidString: ckTimeLog.recordID.recordName)!
-                
-                let fetchedTimeLog = timeLogRepository.withUUID(uuid: uuid).value!
-                
-                if let timeLog = fetchedTimeLog {
-                    NSLog("Skipped already existing recored with UUID \(timeLog.uuid)")
-                    continue
-                }
-                
-                let timeLogData =
-                    TimeLogData(
-                            UUID: uuid,
-                            Activity: activity,
-                            From: from,
-                            Until: until,
-                            CloudSyncPending: false,
-                            CloudSyncStatus: .Unchanged
-                        )
-                
-                let result = timeLogRepository.addNew(timeLogData)
-                
-                if !result.isSucessful {
-                    NSLog("Error persisting timeLog with uuid \(uuid)")
-                } else {
-                    NSLog("Successfully persisted timeLog with uuid \(uuid)")
-                }
-            }
-            
-            if let delegate = self.dataSyncCompletedDelegate {
-                delegate.dataSyncCompleted()
-            }
-        })
-        
+        TimeLogsInCKDownload(
+                dataSyncCompletedDelegate: dataSyncCompletedDelegate,
+                cloudKitContainer: cloudKitContainer
+            ).downloadAndImportTimeLogs()
     }
 }
--- PersonalProductivityAssistant/TimeLogsInCKDownload.swift
+++ PersonalProductivityAssistant/TimeLogsInCKDownload.swift
@@ -7,3 +7,72 @@
 //
 
 import Foundation
+import CloudKit
+
+class TimeLogsInCKDownload {
+    
+    private var dataSyncCompletedDelegate: CKDataSyncCompletedDelegate?
+    private var cloudKitContainer: CKContainer
+    
+    init(dataSyncCompletedDelegate: CKDataSyncCompletedDelegate?, cloudKitContainer: CKContainer) {
+        self.dataSyncCompletedDelegate = dataSyncCompletedDelegate
+        self.cloudKitContainer = cloudKitContainer
+    }
+    
+    public func downloadAndImportTimeLogs() {
+        
+        let predicate = NSPredicate(value:true)
+        let query = CKQuery(recordType: TimeLogsInCK.RecordTypeTimeLogs, predicate: predicate)
+        
+        cloudKitContainer.privateCloudDatabase.perform(query, inZoneWith: nil, completionHandler: { (records, error) in
+            
+            if let error = error {
+                NSLog("Downloading from iCloud failed: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let records = records else { return }
+            
+            NSLog("CK - fetched \(records.count)")
+            
+            let timeLogRepository = TimeLogRepository()
+            
+            for ckTimeLog in records {
+                
+                let activity = ckTimeLog.object(forKey: "activity") as? String ?? ""
+                let from = ckTimeLog.object(forKey: "from") as? Date ?? Date()
+                let until = ckTimeLog.object(forKey: "until") as? Date ?? Date()
+                let uuid = UUID.init(uuidString: ckTimeLog.recordID.recordName)!
+                
+                let fetchedTimeLog = timeLogRepository.withUUID(uuid: uuid).value!
+                
+                if let timeLog = fetchedTimeLog {
+                    NSLog("Skipped already existing recored with UUID \(timeLog.uuid)")
+                    continue
+                }
+                
+                let timeLogData =
+                    TimeLogData(
+                        UUID: uuid,
+                        Activity: activity,
+                        From: from,
+                        Until: until,
+                        CloudSyncPending: false,
+                        CloudSyncStatus: .Unchanged
+                )
+                
+                let result = timeLogRepository.addNew(timeLogData)
+                
+                if !result.isSucessful {
+                    NSLog("Error persisting timeLog with uuid \(uuid)")
+                } else {
+                    NSLog("Successfully persisted timeLog with uuid \(uuid)")
+                }
+            }
+            
+            if let delegate = self.dataSyncCompletedDelegate {
+                delegate.dataSyncCompleted()
+            }
+        })
+    }
+}
