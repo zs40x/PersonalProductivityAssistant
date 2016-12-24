0677edb845bb2ece42444fec9554823fa54e4996
Stefan Mehnert <StefanMehnert@gmail.com>
2016-12-24 05:30:23 +0100

    Changed unit-test projects iOS target version to 10.0, added an empty test class

---
+++ PersonalProductivityAssistantTests/DateRange_asString.swift
@@ -0,0 +1,20 @@
+//
+//  DateRange_asString.swift
+//  PersonalProductivityAssistant
+//
+//  Created by Stefan Mehnert on 24/12/2016.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import XCTest
+@testable import PersonalProductivityAssistant
+
+
+class DateRange_asStringTest: XCTestCase {
+    
+    
+    func testSameDay_onlyTheFirstDate() {
+        
+        
+    }
+}
