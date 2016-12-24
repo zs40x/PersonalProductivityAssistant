6691330d05bd8b93c3bc2580a4eb4bd5e4ac7fa0
Stefan Mehnert <StefanMehnert@gmail.com>
2016-12-24 05:52:16 +0100

    Second failing test, to day in the same month - should not display the month and year twice

--- PersonalProductivityAssistantTests/DateRange_asString.swift
+++ PersonalProductivityAssistantTests/DateRange_asString.swift
@@ -12,12 +12,19 @@ import XCTest
 
 class DateRange_asStringTest: XCTestCase {
     
-    
     func testSameDay_onlyTheFirstDate() {
         
         let christmasDay = Date.makeDateFromComponents(day: 24, month: 12, year: 2016)
         
         XCTAssertEqual("24.12.2016", exerciseAndReturnFormattedDate(from: christmasDay, until: christmasDay))
+    }
+    
+    func testTowDaysInSameMonth_bothDaysButMonthAndYeaeOnlyOnce() {
+        
+        let firstOfMonth = Date.makeDateFromComponents(day: 1, month: 12, year: 2016)
+        let lastOfMonth = Date.makeDateFromComponents(day: 31, month: 12, year: 2016)
+        
+        XCTAssertEqual("1. - 31.12.2016", exerciseAndReturnFormattedDate(from: firstOfMonth, until: lastOfMonth))
     }
     
     private func exerciseAndReturnFormattedDate(from: Date, until: Date) -> String {
