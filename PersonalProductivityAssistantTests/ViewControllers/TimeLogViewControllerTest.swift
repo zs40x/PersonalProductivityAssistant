0341d5be37c4b024d14534774824d04083998f13
Stefan Mehnert <StefanMehnert@gmail.com>
2016-12-25 11:35:37 +0100

    Added empty TimeLogViewControllerTest

---
+++ PersonalProductivityAssistantTests/ViewControllers/TimeLogViewControllerTest.swift
@@ -0,0 +1,9 @@
+//
+//  TimeLogViewControllerTest.swift
+//  PersonalProductivityAssistant
+//
+//  Created by Stefan Mehnert on 25/12/2016.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
