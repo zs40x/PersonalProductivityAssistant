a902241ea83da59051f1949e1ac6f1906768eb12
Stefan Mehnert <StefanMehnert@gmail.com>
2016-12-27 08:24:51 +0100

    Extracted initialization of the dayView into a seperate class

---
+++ PersonalProductivityAssistant/ViewControllers/CalendarDayView.swift
@@ -0,0 +1,9 @@
+//
+//  CalendarDayView.swift
+//  PersonalProductivityAssistant
+//
+//  Created by Stefan Mehnert on 27/12/2016.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
