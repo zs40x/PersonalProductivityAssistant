603d4bd851c2c79e7f4de7000da4f35f79f4ffd3
Stefan Mehnert <StefanMehnert@gmail.com>
2016-12-28 11:55:04 +0100

    Added a test class for the mainViewController

---
+++ PersonalProductivityAssistantTests/ViewControllers/MainViewControllerTest.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewControllerTest.swift
+//  PersonalProductivityAssistant
+//
+//  Created by Stefan Mehnert on 28/12/2016.
+//  Copyright © 2016 Stefan Mehnert. All rights reserved.
+//
+
+import XCTest
+@testable import PersonalProductivityAssistant
+
+
+class MainViewControllerTest: XCTestCase {
+    
+    var viewController = MainViewController()
+    
+    
+    override func setUp() {
+        super.setUp()
+        
+        viewController =
+            UIStoryboard(name: "Main", bundle: nil)
+                .instantiateViewController(withIdentifier: "MainView") as! MainViewController
+        
+        let _ = viewController.view
+    }
+}
